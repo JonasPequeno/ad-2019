334c112e194a0cb254f362b651a829d04298fa42
JonasPequeno <jonaspdasilva31@gmail.com>
2020-06-05 19:07:54 -0300

    Adicionado rotas de person e draw

--- backend/src/config/app.js
+++ backend/src/config/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('./cors');
-
+const PersonRouter = require('../routes/person.router');
+const DrawRouter = require('../routes/draw.router');
 const app = express();
+const mongoose = require('mongoose');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 7000);
 app.use(cors);
+
+mongoose.set('useFindAndModify', false);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/draw', DrawRouter);
+app.use('/api/person', PersonRouter);
 
 module.exports = app;
