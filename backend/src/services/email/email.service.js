fe0b64f7ebf0c3f8cefb080c2fabbbb1c0b44e29
JonasPequeno <jonaspdasilva31@gmail.com>
2020-06-05 19:09:10 -0300

    Criado serviço para o envio de email

---
+++ backend/src/services/email/email.service.js
@@ -0,0 +1,23 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('');
+
+
+exports.sendEmail = (personName, to, friendName) => {
+    const msg = {
+        to,
+        from: 'jonaspdasilva31@gmail.com',
+        subject: 'Amigo secreto',
+        text: 'Amigo secreto - ad-2019',
+        html: `           
+            Olá <strong>${personName}</strong> seu amigo secreto é <strong>${friendName}</strong>
+        `,
+    };
+    sgMail.send(msg)
+        .then(res => {
+            return res;
+        })
+        .catch(err => {
+            return err;
+        })
+};
+
